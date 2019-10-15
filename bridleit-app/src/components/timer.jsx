4288f01b480c6f38d8c538c2d45c719b15757710
Luke Lopez <xlucas14@gmail.com>
2019-10-15 18:06:10 -0400

    Fleshed out timer comp

--- bridleit-app/src/components/timer.jsx
+++ bridleit-app/src/components/timer.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Timer = ({ value }) => {
-  return <h1>{value}</h1>;
-};
+class Timer extends Component {
+  state = { remainingTime: 0, timerActive: this.props.timerActive };
+
+  componentDidMount() {
+    setInterval(this.update, 1000);
+  }
+
+  update = () => {
+    const { remainingTime, timerActive } = this.state;
+    const { startingTime, onTimeOut } = this.props;
+
+    if (timerActive) {
+      if (remainingTime < 1) {
+        this.setState({ remainingTime: startingTime });
+        onTimeOut();
+      } else {
+        const newTime = remainingTime - 1;
+        this.setState({ remainingTime: newTime });
+      }
+    }
+  };
+
+  render() {
+    const { remainingTime } = this.state;
+    return <h1>{remainingTime}</h1>;
+  }
+}
 
 export default Timer;
